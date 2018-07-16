240ec0f0fafc1c4821786e4371c63a1694ea2415
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 19:57:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,7 +9,8 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 def won?(board)
 
   WIN_COMBINATIONS.each do |combo|
-    return combo if (board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]])
+    return combo if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
+    board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O" )
   end
 
 end
