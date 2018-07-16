c62f4fa57147a5649ab24743f45688b497ae9695
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 19:57:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,6 +11,8 @@ def won?(board)
   WIN_COMBINATIONS.each do |combo|
     return combo if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
     board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O" )
+  else
+    return false
   end
 
 end
